257320aff8657bfe20b54c9d2ae02ff607177697
mjaumi <mjaumi2864@gmail.com>
2023-05-28 05:21:01 +0600

    public and private route added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import VideoPlayer from './pages/VideoPlayer/VideoPlayer';
 import Login from './pages/Authentication/Login/Login';
 import Signup from './pages/Authentication/Signup/Signup';
+import PrivateRoute from './components/Routes/PrivateRoute/PrivateRoute';
+import PublicRoute from './components/Routes/PublicRoute/PublicRoute';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/videos/:videoId' element={<VideoPlayer />} />
+        <Route path='/' element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        } />
+        <Route path='/signup' element={
+          <PublicRoute>
+            <Signup />
+          </PublicRoute>
+        } />
+        <Route path='/videos/:videoId' element={
+          <PrivateRoute>
+            <VideoPlayer />
+          </PrivateRoute>
+        } />
       </Routes>
     </Router>
   );
